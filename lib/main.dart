530bcd1ce89f1c675428181a396491b138613556
salemzubair0-pixel <salemzubair@gmail.com>
2025-11-25 21:27:51 +0300

    after1

---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(TodoApp());
+}
+
+class TodoApp extends StatefulWidget {
+  @override
+  _TodoAppState createState() => _TodoAppState();
+}
+
+class _TodoAppState extends State<TodoApp> {
+  final List<String> _todos = [] ;
+
+  void _addTodoItem(String task) {
+    if(task.isNotEmpty){
+      setState(() {
+        _todos.add(task);
+      });
+    }
+  }
+
+  void _removeTodoItem(int index){
+    setState(() {
+      _todos.removeAt(index);
+    });
+  }
+
+  @override 
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Simple To-Do App'),
+        ),
+        body: Column(
+          children: <Widget>[
+            TextField(
+              onSubmitted: _addTodoItem,
+              decoration: InputDecoration(
+                labelText: 'Enter a new task'
+              ),
+            ),
+            Expanded(
+              child: ListView.builder(
+              itemCount: _todos.length,
+              itemBuilder: (context, index){
+                return ListTile(
+                  title: Text(_todos[index]),
+                  trailing: IconButton(
+                    onPressed: () => _removeTodoItem(index), 
+                    icon: const Icon(Icons.delete)),
+                );
+              } ,
+              )
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
